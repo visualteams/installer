33791ded6a75030b30b09b86d7b31c5444b748d4
Sylvain Chauffaille <contact@sylchauf.net>
2021-09-15 15:50:37 +0200

    Remove sudo

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
+if ((EUID != 0)); then
+    echo "Root or Sudo  Required for script ( $(basename $0) )"
+    exit
+fi
+
 chmod 777 configure-settings.sh
 
 ./configure-settings.sh
 
 export METEOR_SETTINGS=$(cat settings.json)
 
-sudo docker-compose up
+docker-compose up
